a480c6d29faa91cfbd065b97f6e630cb4c18e43e
IamLikhon <likhonadnan55@gmail.com>
2023-03-27 14:30:12 +0600

    done today

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
-    console.log(props);
-    const {name,category,price,img,ratings,ratingsCount,seller}=props.product
+    // console.log(props);
+    const {name,price,img,ratings,seller}=props.product
+    // ei khane read kore patha te hobe 
+    const handelAddToCart=props.handelAddToCart
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,7 +18,11 @@ const Product = (props) => {
             <p>Manufacturer:{seller}</p>
             <p>Rating:{ratings} Star</p>
             </div>
-            <button className='cartAdd'>Add To Cart</button>
+            <button onClick={()=>handelAddToCart(props.product)} 
+            className='cartAdd'>
+                Add To Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -3,19 +3,29 @@ import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts]=useState([])
-
+    const [cart, setCart]=useState([])
+    
     useEffect(()=>{
         fetch(`products.json`)
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
+    // handel ti pathano hoise nicce dike 
+    const handelAddToCart=(product)=>{
+        // console.log(product);
+        const newCart=[...cart,product]
+        setCart(newCart)
+    }
     return (
         <div className='shopContainer'>
             <div className='productsContainer'>
                 {
                     products.map(product => <Product
                         key={product.id}
-                        product={product}>
+                        product={product}
+                        // ei khane call korte hobe 
+                        handelAddToCart={handelAddToCart}
+                        >
                                
                     </Product>)
 
@@ -23,6 +33,7 @@ const Shop = () => {
             </div>
             <div className='cartContainer'>
                 <h2>Cart container</h2>
+                <p>Selected Items:{cart.length}</p>
             </div>
             
         </div>
