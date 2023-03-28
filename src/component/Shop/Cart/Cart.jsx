41e288b23dab73a11bf0df33cd082b777ca9266e
IamLikhon <likhonadnan55@gmail.com>
2023-03-28 11:20:32 +0600

    added the show cart

--- src/component/Shop/Cart/Cart.jsx
+++ src/component/Shop/Cart/Cart.jsx
@@ -6,16 +6,19 @@ const Cart = (props) => {
 
     let total=0;
     let totalShipping=0;
+    let quantity=0;
     for(const product of cart){
-        total=total+product.price
+        product.quantity= product.quantity||1;
+        total=total+product.price * product.quantity
         totalShipping=totalShipping+product.shipping
+        quantity=quantity+product.quantity
     }
     let tax=total*7/100;
     const grandTotal=tax+total+totalShipping
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
-                <p>Selected Items:{cart.length}</p>
+                <p>Selected Items:{quantity}</p>
                 <p>Total Price: ${total} </p>
                 <p>Total Shipping Price: ${totalShipping} </p>
                 <p>Tax: ${tax.toFixed(2)} </p>
